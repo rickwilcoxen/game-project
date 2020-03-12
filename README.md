eb67f87cdd492023481f8efa4ccc03c4f3de8d9f
rickwilcoxen <rickwilcoxen95@gmail.com>
2020-03-12 15:27:46 -0400

    First commit
    Added index.html, created simplified game board
    Added app.js with code that allows x & o to be added to the board,
    and invalid clicks to not register (aka, x / o will not be overwritten
    with multiple clicks)
    Readme empty, added placeholder
    Added index.scss with some colors just to tell difference between box
    and bg on browser.

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+(づ￣ ³￣)づ <( here's the readme! )
+
+
+
+¯\_(ツ)_ /¯  
+oop its empty
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,5 +7,49 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+//toe = either x || o
+let toe = 'x'
+
+  //get the position of the click & add x or o
+  const addToe=function(event){
+  const clickPosition = event.target.id
+
+  //if box A is clicked, console logs A+positionID
+  //spot is the text(x or o) on div
+  console.log(toe + clickPosition)
+  const spot = $(event.target).text()
+
+  //once position is found, log X or O in text
+  if(spot !== 'x' || 'o' ) {
+    $(event.target).text(toe)
+
+  } //else if (event.target === 'x')
+    if (toe === 'x') {
+      toe = 'o'
+    } else {
+      //if (toe === 'o')
+      toe = 'x'
+    } //if spot is occupied dont put x or o! /turn off click
+     if (spot === 'x' || 'o') {
+     console.log('nope') //still changes even if invalid
+  //  $('.box' ).off('click')
+    }
+  }
+//  document.getElementById('#.box').textContent="toe";
+
+
+
+
+  //if box is clicked, get the position of click (ClickPosition)
+
+  //after getting ClickPosition - change textContent to toe= "X"
+    //change toe = "O";
+    //
+    //if previous spot clicked was X, add O
+    //if previous spot clicked was O, add X
+  //if spot is invalid, do nothing
+
+//click event
+$('.box').on('click', addToe)
+
 })
