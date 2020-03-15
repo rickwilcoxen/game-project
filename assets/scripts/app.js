a3eb12bc739fbc8133df71dd03825f7c0f482774
rickwilcoxen <rickwilcoxen95@gmail.com>
2020-03-14 22:21:36 -0400

    Fixed bug in invalid click
    Fix the bug that allowed invalid clicks (actually this time.)
    Moved all code initially in app.js to events.js and cleaned it up
    a bit.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,7 +6,10 @@ const authEvents = require('./auth/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+
+
 $(() => {
 
 
+$('.box').on('click', authEvents.addToe)
 })
--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -1,49 +1,7 @@
 'use strict'
+const api = require('./api.js')
 
-
-//toe = either x || o
 let toe = 'x'
-
-  //get the position of the click & add x or o
-  const addToe=function(event){
-  const clickPosition = event.target.id
-
-  //if box A is clicked, console logs A+positionID
-  //spot is the text(x or o) on div
-  console.log(toe + clickPosition)
-  const spot = $(event.target).text()
-
-  //once position is found, log X or O in text
-  if(spot !== 'x' || 'o' ) {
-    $(event.target).text(toe)
-
-  } //else if (event.target === 'x')
-    if (toe === 'x') {
-      toe = 'o'
-    } else {
-      //if (toe === 'o')
-      toe = 'x'
-    } //if spot is occupied dont put x or o! /turn off click
-     if (spot === 'x' || 'o') {
-     console.log('nope') //still changes even if invalid
-  //  $('.box' ).off('click')
-    }
-  }
-//  document.getElementById('#.box').textContent="toe"
-
-  //if box is clicked, get the position of click (ClickPosition)
-
-  //after getting ClickPosition - change textContent to toe= "X"
-    //change toe = "O";
-    //
-    //if previous spot clicked was X, add O
-    //if previous spot clicked was O, add X
-  //if spot is invalid, do nothing
-
-//click event
-$('.box').on('click', addToe)
-
-
 //create an empty board in js
 let board = [
   '', '', '',
@@ -51,25 +9,39 @@ let board = [
   '', '', ''
 ]
 //create a way to keep track of current player
-let user = 'Player 1'
+let player = 'Player 1'
 
 //add current play on js representation of board
   //current player changes between x/o
   //cannot choose already occupied spots
-
-
+  const addToe = function(event){
+  const clickPosition = event.target.id
+  //spot is the text(x or o) on div
+  console.log(toe + clickPosition)
+  const spot = $(event.target).text()
+  //once position is found, log X or O in spot
+  if(spot !== 'x' && spot !== 'o' ) {
+    $(event.target).text(toe)
+    if (toe === 'x') {
+      toe = 'o'
+    } else {
+      toe = 'x'
+    }
+  } if (spot === 'x' && spot === 'o'){
+      console.log('nope')
+    }
+  }
 //check board for winner
-const onAddToe = function (){
+//const onAddToe = function (){
   //log plays to an array(board)vvv
   //when user adds x/o to board, find position of it (id#)
+
   //use that # as the # in the array (0-8)
+
   //then vvvv
   //does array match any of the 16 ways to win? y/n
   //if no - game over - cats win!
-}
-
-
+//}
 module.exports = {
-  addToe,
-  onAddToe,
+  addToe
 }
